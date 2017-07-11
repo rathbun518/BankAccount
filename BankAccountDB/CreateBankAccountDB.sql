5cbb0cf05b14f65cef0f84f2e5e11e822bc24e22
rrathbun <rrathbun@B11-R111E-15>
2017-07-11 14:12:16 -0700

    Created database script with customer table

--- BankAccountDB/CreateBankAccountDB.sql
+++ BankAccountDB/CreateBankAccountDB.sql
@@ -1,1 +1,14 @@
-﻿
+﻿CREATE DATABASE BankDB
+GO
+
+USE BankDB
+GO
+
+CREATE TABLE Customer
+(
+	AccountNo	int	PRIMARY KEY	IDENTITY(10000,1)
+	,FirstName	varchar(50)		NOT NULL
+	,LastName	varchar(50)		NULL
+	,Address	varchar(150)	NOT NULL
+	,Email		varchar(300)	NOT NULL 
+)
